2f5eaf6191a966b3ef252f7f9d7571585f29952f
Vikas Kumar <nego.vikash925@gmail.com>
2020-05-01 02:19:47 +0530

    Update Article Component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,21 +1,56 @@
 import React from 'react';
 
 class Articles extends React.Component {
+  state = {
+    articles:[],
+    input:'',
+    click: false
+  }
+  onChangeHandler = (event) => {
+    const value = event.target.value;
+    this.setState({input: value});
+  }
+  onFetchHandler =  (event) => {
+    this.setState({click: true});
+    const input = this.state.input;
+    let url = 'https://jsonmock.hackerrank.com/api/articles?author='+input+'&page=1';
+    fetch(url)
+      .then(res => res.json())
+      .then(response => {
+        if(response.total) {
+          let articles = response.data.slice(0, 4);
+          this.setState({ articles : articles});
+        }
+      })
+      .catch(err=> {
+        console.log(err.message)
+      })
+
+  }
 
   render() {
+    let results;
+    if (this.state.articles.length) {
+      let articles = this.state.articles.map((article, index) => {
+        return <li key={index} data-testid="result-row">{article.title}</li>
+      })
+      results = <div className="results">{articles}</div>;
+    } else {
+      results = <div data-testid="no-results">No results</div>;
+    }
+    if(!this.state.click) {
+      results ='';
+    }
     return (
       <React.Fragment>
         <div className="controls">
           <div className="input-container">
             <span>author:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
-            <button className="fetch-button" data-testid="fetch-button">Fetch</button>
+            <input type="text" className="text-input" onChange = {this.onChangeHandler} data-testid="text-input" />
+            <button className="fetch-button" onClick={this.onFetchHandler} data-testid="fetch-button">Fetch</button>
           </div>
         </div>
-        <div className="results">
-          <li key="example-key" data-testid="result-row">Example title</li>
-        </div>
-        <div data-testid="no-results">No results</div>
+        {results}
       </React.Fragment>
     );
   }
